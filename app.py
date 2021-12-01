b937a60f2f08843c43f51fc489ada891cea84939
Pjotrs OSnovskis <pjotrs.osnovskis@gmail.com>
2021-12-01 20:20:54 +0000

    fix: remove dbconfig.py and moved all db config to app.py for convenience

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
 from flask import Flask
 from flask_restful import reqparse, Api, Resource
+from flask-mysqldb import MySQL
 
 app = Flask(__name__)
 
+app.config['MYSQL_USER'] = 'sql4455717'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_HOST'] = 'sql4.freemysqlhosting.net'
+app.config['MYSQL_DB'] = 'sql4455717'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+
+mysql = MySQL(app)
--- dbconfig.py
+++
@@ -1,9 +0,0 @@
-from app import app
-from flaskext.mysql import MySQL
-
-MySql = MySQL()
-app.config['DATABASE_USER'] = 'root'
-app.config['DATABASE_PASSWORD'] = ''
-app.config['DATABASE_DB'] = 'contact_details'
-app.config['DATABASE_HOST'] = 'localhost'
-MySql.init_app(app)
