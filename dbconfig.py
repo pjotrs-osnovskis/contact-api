7f6004effb50f5960991f59cd69815d643bedbdf
Pjotrs OSnovskis <pjotrs.osnovskis@gmail.com>
2021-12-01 20:08:09 +0000

    fix[dbconfig.py]: remove password for database as not needed at the moment

--- dbconfig.py
+++ dbconfig.py
@@ -3,7 +3,7 @@ from flaskext.mysql import MySQL
 
 MySql = MySQL()
 app.config['DATABASE_USER'] = 'root'
-app.config['DATABASE_PASSWORD'] = 'root'
+app.config['DATABASE_PASSWORD'] = ''
 app.config['DATABASE_DB'] = 'contact_details'
 app.config['DATABASE_HOST'] = 'localhost'
 MySql.init_app(app)
