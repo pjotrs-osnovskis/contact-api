7f52ac774d43fad275f6c73d03753d99d42f3651
Pjotrs OSnovskis <pjotrs.osnovskis@gmail.com>
2021-12-02 16:59:37 +0000

    fix: changed to flask-SqlAlchemy for convenience!!

--- app.py
+++ app.py
@@ -1,13 +1,8 @@
-from flask import Flask, request
-from flask_restful import reqparse, Api, Resource
-from flask_mysqldb import MySQL
+from flask import Flask, request, jsonify, make_response
+from flask_sqlalchemy import SQLAlchemy
+from marshmallow_sqlalchemy import ModelSchema
+from marshmallow import fields
 
 app = Flask(__name__)
-
-app.config['MYSQL_USER'] = 'sql4455717'
-app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_HOST'] = 'sql4.freemysqlhosting.net'
-app.config['MYSQL_DB'] = 'sql4455717'
-app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
-
-mysql = MySQL(app)
+app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://sql4455717:Sa8jeBNmvl@sql4.freemysqlhosting.net:3306/sql4455717'
+db = SQLAlchemy(app)
