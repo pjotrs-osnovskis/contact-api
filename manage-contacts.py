1e580d945073addfd773e5dbf1ddd00a94cd3ed6
Pjotrs OSnovskis <pjotrs.osnovskis@gmail.com>
2021-12-04 12:57:30 +0000

    feat: add functionality to view single contact by ID

--- manage-contacts.py
+++ manage-contacts.py
@@ -1,9 +1,11 @@
 from app import Contact, db, contacts_schema
+from flask import request
 
-print("1 to view all contacts")
-print("2 to create a contact")
-print("3 to delete by ID")
-print("4 to update by ID")
+print("1 to VIEW ALL contacts")
+print("2 to SHOW contact by ID")
+print("3 to CREATE a contact")
+print("4 to UPDATE contact by ID")
+print("5 to DELETE contact by ID")
 option = input("Select option: ")
 
 if option == "1":
@@ -11,6 +13,17 @@ if option == "1":
     print(contacts_schema.dump(contacts))
 
 elif option == "2":
+    selected_id = input("Enter contact ID to view: ")
+    contact = Contact.query.get_or_404(selected_id)
+    if selected_id == str(contact.id):
+        print("Fist Name: " + contact.first_name)
+        print("Last Name: " + contact.last_name)
+        print("Email: " + contact.email)
+        print("Phone Number: " + contact.phone_number)
+    else:
+        print("Contact not found!")
+
+elif option == "3":
     f_name = input("Enter First Name: ")
     l_name = input("Enter Last Name: ")
     email = input("Enter Email: ")
@@ -19,9 +32,24 @@ elif option == "2":
     contact = Contact(first_name=f_name, last_name=l_name, email=email, phone_number=phone_nr)
     db.session.add(contact)
     db.session.commit()
-    
-elif option == "3":
-    selected_id = input("Enter selected id to delete: ")
+
+# elif option == "4":
+#     selected_id = input("Enter contact ID to UPDATE: ")
+#     contact = Contact.query.get_or_404(selected_id)
+#     print("Fist Name: " + contact.first_name)
+#     print("Last Name: " + contact.last_name)
+#     print("Email: " + contact.email)
+#     print("Phone Number: " + contact.phone_number)
+    # f_name = input("Enter First Name: ")
+    # l_name = input("Enter Last Name: ")
+    # email = input("Enter Email: ")
+    # phone_nr = input("Enter Phone Number: ")
+    # updated_contact = Contact(first_name=f_name, last_name=l_name, email=email, phone_number=phone_nr)
+    # db.session.update(updated_contact)
+    # db.session.commit()
+
+elif option == "5":
+    selected_id = input("Enter contact ID to DELETE: ")
     contact = Contact.query.get_or_404(selected_id)
     db.session.delete(contact)
     db.session.commit()
