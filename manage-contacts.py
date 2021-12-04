d0bcbbd9e3beac8a495ad96097926e7cff021f8f
Pjotrs OSnovskis <pjotrs.osnovskis@gmail.com>
2021-12-04 12:24:38 +0000

    fix: added menu to select list, create and delete contacts, feat: add delete by ID function

--- manage-contacts.py
+++ manage-contacts.py
@@ -1,10 +1,26 @@
-from app import Contact, db
+from app import Contact, db, contacts_schema
 
-f_name = input("Enter First Name: ")
-l_name = input("Enter Last Name: ")
-email = input("Enter Email: ")
-phone_nr = input("Enter Phone Number: ")
+print("1 to view all contacts")
+print("2 to create a contact")
+print("3 to delete by ID")
+print("")
+print("")
+option = input("Select option: ")
 
-contact = Contact(first_name=f_name, last_name=l_name, email=email, phone_number=phone_nr)
-db.session.add(contact)
-db.session.commit()
+if option == "1":
+    print("You have selected option 1")
+elif option == "2":
+    f_name = input("Enter First Name: ")
+    l_name = input("Enter Last Name: ")
+    email = input("Enter Email: ")
+    phone_nr = input("Enter Phone Number: ")
+
+    contact = Contact(first_name=f_name, last_name=l_name, email=email, phone_number=phone_nr)
+    db.session.add(contact)
+    db.session.commit()
+elif option == "3":
+    selected_id = input("Enter selected id to delete: ")
+    contact = Contact.query.get_or_404(selected_id)
+    db.session.delete(contact)
+    db.session.commit()
+    print(f'Contact {contact.first_name} deleted')
