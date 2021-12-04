40fadd4fce342f20b02bd6cb5fa17ca884acb8cf
Pjotrs OSnovskis <pjotrs.osnovskis@gmail.com>
2021-12-04 18:38:20 +0000

    feat: add validation for first and last names, email and phone number

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
+from enum import unique
 from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import validates 
 from flask_marshmallow import Marshmallow
+from marshmallow import fields, ValidationError
 from flask_restful import Api, Resource
 import os
 if os.path.exists("env.py"):
     import env
+from sqlalchemy.orm import validates
+import re 
     
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']=os.environ.get("DB_URL")
@@ -16,11 +21,49 @@ api = Api(app)
 #### Models ####
 class Contact(db.Model):
     __tablename__ = "contacts"
-    id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String(20))
-    last_name = db.Column(db.String(20))
-    email = db.Column(db.String(100))
-    phone_number = db.Column(db.String(10))
+    id = db.Column(db.Integer, primary_key=True, unique=True)
+    first_name = db.Column(db.String(), index=True, nullable=False)
+    last_name = db.Column(db.String(), index=True, nullable=False)
+    email = db.Column(db.String(), index=True, nullable=False)
+    phone_number = db.Column(db.String(13), index=True, nullable=False)
+    
+    @validates('first_name') 
+    def validate_first_name(self, key, first_name):
+        if not first_name:
+            raise AssertionError('No first name provided')
+        if len(first_name) < 3 or len(first_name) > 20:
+            raise AssertionError('First name must be between 3 and 20 characters')
+        return first_name
+    
+    @validates('last_name') 
+    def validate_last_name(self, key, last_name):
+        if not last_name:
+            raise AssertionError('No last name provided')
+        if len(last_name) < 3 or len(last_name) > 20:
+            raise AssertionError('Last name must be between 3 and 20 characters')
+        return last_name
+
+    @validates('email') 
+    def validate_email(self, key, email):
+        if not email:
+            raise AssertionError('No email provided')
+        if not re.match("[^@]+@[^@]+\.[^@]+", email):
+            raise AssertionError('Please enter correct email')
+        if Contact.query.filter(Contact.email == email).first():
+            raise AssertionError('Email is already in use')
+        return email
+
+    @validates('phone_number') 
+    def validate_phone_number(self, key, phone_number):
+        validate = "r'^(?:\+?0044)?[007]\d{9,15}$'"
+        if not phone_number:
+            raise AssertionError('No phone number provided')
+        if len(phone_number) < 9 or len(phone_number) > 15:
+            raise AssertionError('(C1) Enter correct phone number, following inner UK standard (07) or International (0044)')
+        if re.search(validate, phone_number):
+            raise AssertionError('(C2) Enter correct phone number, following inner UK standard (07) or International (0044)') 
+
+        return phone_number
 
     def __repr__(self):
         return '' % self.id
@@ -89,4 +132,4 @@ class ContactResource(Resource):
 api.add_resource(ContactResource, '/contacts/<int:contact_id>')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
